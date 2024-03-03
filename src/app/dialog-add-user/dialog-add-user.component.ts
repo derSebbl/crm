60310b97f8f35b562ff6166777e1dd53b916d10b
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-03 15:50:43 +0100

    implement firestore

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { User } from '../../models/user.class';
-import { Firestore, addDoc, collection, getDocs, query } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -9,14 +10,24 @@ import { Firestore, addDoc, collection, getDocs, query } from '@angular/fire/fir
 })
 export class DialogAddUserComponent {
   
+  firestore: Firestore = inject(Firestore);
 
-  constructor(private firestore: Firestore) { }
+  constructor() {
+
+   }
 
   user= new User();
   birthDate!: Date;
 
   saveUser() {
+    let acollection = collection(this.firestore, 'users');
     this.user.birthDate = this.birthDate?.getTime() ?? 0;
     console.log(this.user);
+
+    addDoc(acollection, this.user.toJSON()).then((docRef) => {
+      console.log("user added with ID: ", docRef.id);
+    }).catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 }
 }
