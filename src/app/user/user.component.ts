e3c11052732fcb628e74f6321bee6d91876158d4
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-04 20:46:55 +0100

    added firestore to Dashboard to show users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class'
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -10,10 +14,19 @@ import { User } from '../../models/user.class'
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  firestore: Firestore = inject(Firestore);
+  users$!: Observable<any[]>;
 
   user= new User();
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) {
+    const aCollection = collection(this.firestore, 'users')
+    this.users$ = collectionData(aCollection);
+   }
+
+  ngOnInit(): void{
+
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
