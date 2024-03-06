670c978f6f3c69eb2633a9afc16f6f0f8ca824c3
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-06 20:46:37 +0100

    added user object and details from firestore

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, doc, getDoc } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,13 +11,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent {
 
+  firestore: Firestore = inject(Firestore);
+  users$!: Observable<any[]>;
+
   userId = '';
+  user = new User();
 
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
       this.userId = paramMap.get('id')!;
+      this.getUser();
   })
+  }
+
+  async getUser(){
+    const userDoc = doc(this.firestore, 'users', this.userId);
+    const userSnapshot = await getDoc(userDoc);
+
+    if (userSnapshot.exists()) {
+      this.user = new User();
+      Object.assign(this.user, userSnapshot.data());
+    } else {
+      console.log('No such user!');
+    }
   }
 }
