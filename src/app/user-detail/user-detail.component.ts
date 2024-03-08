54b4c9dd5e508b5aaca8b15762547e5276b3803d
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-08 17:51:11 +0100

    change instanz of user.
    copy the json instead of open it itself.

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -44,11 +44,11 @@ export class UserDetailComponent {
 
   openAdressDialog(){
     const dialog = this.dialog.open(DialogEditAddressComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 
   editUserDetail(){
     const dialog = this.dialog.open(DialogEditUserComponent)
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 }
