9dfd7690493442506208bc096af3eeb74e34a63d
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-10 15:41:40 +0100

    added two charts with hardcode

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,78 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  chart1: any;
+  customers = [1, 56, 334, 34, 15];
+  months = ['January', 'February', 'March', 'April', 'May'];
+
+  
+  chart2: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.chart1 = new Chart('chart1', {
+      type: 'line', 
+      data: {
+        labels: this.months,
+        datasets: [{
+          label: 'Customers',
+          data: this.customers,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+          },
+        },
+        interaction: {
+          intersect: false,
+        },
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true
+            }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Customers'
+            },
+            suggestedMin: -10,
+            suggestedMax: 200
+          }
+        }
+      },
+    });
+
+    this.chart2 = new Chart('chart2', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 
 }
