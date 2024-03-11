c63a26f7b0654031768b05c97ae7ba2d31ee92c3
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-11 19:40:41 +0100

    add pie chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import Chart from 'chart.js/auto';
+import { User } from '../../models/user.class';
+
 
 
 
@@ -13,12 +16,33 @@ export class DashboardComponent {
   customers = [1, 56, 334, 34, 15, 23, 45, 67, 78, 89, 90, 100];
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+  user: User = {
+    street: '',
+    zipCode: 0,
+    city: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    pet: '',
+    petInfo: false,
+    toJSON: function (): { firstName: string; lastName: string; email: string; street: string; zipCode: number; city: string; pet: string; petInfo: boolean; } {
+      throw new Error('Function not implemented.');
+    }
+  };
   
   chart2: any;
 
-  constructor() { }
+
+
+  constructor() {
+
+   }
+
+  firestore: Firestore = inject(Firestore);
+
 
   ngOnInit() {
+    
     this.chart1 = new Chart('chart1', {
       type: 'line', 
       data: {
@@ -60,23 +84,16 @@ export class DashboardComponent {
     });
 
     this.chart2 = new Chart('chart2', {
-      type: 'bar',
+      type: 'pie',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Dog', 'Cat'],
         datasets: [
           {
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [2, 3],
             borderWidth: 1,
           },
         ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
       },
     });
   }
