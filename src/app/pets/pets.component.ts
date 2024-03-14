8f560bca60792ef55eb94ee6d8fb89f5f73c86fd
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-14 20:30:23 +0100

    added table for pets and function to set them in the care

--- src/app/pets/pets.component.ts
+++ src/app/pets/pets.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { User } from '../../models/user.class'
+import { Firestore, collectionData, doc, getDoc, updateDoc } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-pets',
@@ -6,5 +13,30 @@ import { Component } from '@angular/core';
   styleUrl: './pets.component.scss'
 })
 export class PetsComponent {
+  firestore: Firestore = inject(Firestore);
+  users$!: Observable<any[]>;
+  user: any;
 
+
+  constructor(public dialog: MatDialog) {
+    const aCollection = collection(this.firestore, 'users')
+    this.users$ = collectionData(aCollection);
+    this.users$ = collectionData(aCollection, {idField: 'id'});
+   }
+
+  ngOnInit(): void{
+  }
+
+  togglePetInfo(userId: string) {
+    const userRef = doc(this.firestore, 'users', userId);
+    getDoc(userRef).then((userDoc) => {
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const currentPetInfo = userData?.['petInfo'];
+        updateDoc(userRef, {
+          petInfo: !currentPetInfo
+        });
+      }
+    });
+  }
 }
