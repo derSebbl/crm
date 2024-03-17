3f3ee33a4ef760378617ae4d3ebc8c059a4ae988
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-03-17 15:33:34 +0100

    added pet care component

--- src/app/pet-care/pet-care.component.ts
+++ src/app/pet-care/pet-care.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { User } from '../../models/user.class';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pet-care',
@@ -7,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class PetCareComponent {
 
+  firestore: Firestore = inject(Firestore);
+  users$!: Observable<any[]>;
+
+  userId = '';
+  user = new User();
+
+  constructor(private route:ActivatedRoute,
+    public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userId = paramMap.get('id')!;
+      this.getUser();
+  })
+  }
+
+  async getUser(){
+    const userDoc = doc(this.firestore, 'users', this.userId);
+    const userSnapshot = await getDoc(userDoc);
+
+    if (userSnapshot.exists()) {
+      this.user = new User();
+      Object.assign(this.user, userSnapshot.data());
+    } else {
+      console.log('No such user!');
+    }
+  }
 }
